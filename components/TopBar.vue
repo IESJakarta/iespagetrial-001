da2d2c870570fc1f95d5ef88da97d67be63bbb20
msrody <mike.rody@gmail.com>
2022-02-06 18:03:03 +0700

    New Repo

---
+++ components/TopBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <IESLogo/>
+    <TopMenu />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <NavBar />
+</div>
+</template>
+
+<script>
+import TopMenu from "~/components/TopMenu.vue";
+import NavBar from "~/components/NavBar.vue";
+export default {
+    name: "IndexPage",
+    components: { TopMenu, NavBar }
+}
+</script>
